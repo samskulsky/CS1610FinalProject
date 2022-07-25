5748f38bbba63054237353e3645d4defc97062ac
Sammy Skulsky <69123082+samskulsky@users.noreply.github.com>
2022-07-25 14:44:50 +0000

    Image saving & resizing

--- Main.java
+++ Main.java
@@ -11,13 +11,16 @@ class Main {
 
 	// moved central variables to top so they can be accessed by all methods
 	public static JFrame frame;
+  public static ImmutableImage mosaicImage;
 	public static BufferedImage image;
-	
+	//xx2
   public static void main(String[] args) {
 		
     System.out.println("Good Morning World");
 
-    program();
+    loadMainImage();
+    mosaicImage = mosaicImage.contrast(2.0); // Testing
+    image = mosaicImage.awt();
     
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
@@ -33,14 +36,39 @@ class Main {
     // Create frame
     frame= new JFrame("Mosaic Maker");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+		/* Was able to display the image in the panel
+     * but had to use BufferedImage instead of ImmutableImage
+     * for now... also, I don't know how we want to deal
+     * with image size because it is currently too big
+     * for browser window.
+		 */
 
-		frame.setSize(image.getWidth(), image.getHeight());
+    /* Image has been resized for display.
+    /* We'll probably do all the mosaic stuff purely using ImmutableImage.
+    /* There's a scrimage operation called Overlay which we can use to cover an image with another image.
+    */
+
+    float imgWidth = image.getWidth(null);
+    float imgHeight = image.getHeight(null);
+    float maxLength = Math.max(imgWidth, imgHeight);
+    int targetSize = 500;
+    int scaledWidth = targetSize * (int) (imgWidth/maxLength);
+    int scaledHeight = targetSize * (int) (imgHeight/maxLength);
+    Image scaledImage = image.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_DEFAULT);
+    
+		//frame.setSize(scaledImage.getWidth(null), scaledImage.getHeight(null));
 		frame.setResizable(false);
-
+       
     JLabel label = new JLabel();
-    label.setIcon(new ImageIcon(image));
+    label.setIcon(new ImageIcon(scaledImage));
     frame.getContentPane().add(label,BorderLayout.CENTER);
 
+    // I'm going to keep this for testing. We could use it later to save the image to a file.
+    JButton startButton= new JButton("Save Image");
+    startButton.setFont(startButton.getFont().deriveFont(20.0f));
+    frame.add(startButton, BorderLayout.PAGE_END);
+    
     frame.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosed(WindowEvent e) {
@@ -55,12 +83,10 @@ class Main {
   }
   
   // there's probably a better name than this
-  private static void program() {
+  private static void loadMainImage() {
     File imageFile = new File("photo.jpeg");
     try {
-			image = ImageIO.read(imageFile);
-			
-      // Mosaic stuff
+      mosaicImage = ImmutableImage.loader().fromFile(imageFile);
 		} catch (IOException e) {
 			System.out.println(e);
 		}
